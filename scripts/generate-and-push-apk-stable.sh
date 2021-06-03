7296422e8d653a2bdf234d535d693539a9fdcda3
Anurag Arwalkar <anruagarwalkar@gmail.com>
2021-06-03 19:35:48 +0530

    removed hardcoding

--- scripts/generate-and-push-apk-stable.sh
+++ scripts/generate-and-push-apk-stable.sh
@@ -3,13 +3,15 @@
 # Please give your path of source files 
 read -p "Press any key to continue pushing android app to Anurag Drive..."
 
+cd ..
+cd "build/app/outputs/flutter-apk"
 flutter build apk 
-mv -f "C:/projects/my_projects/cowin_app/build/app/outputs/flutter-apk/app-release.apk" "C:/Users/AnuragArwalkar/Google Drive/Mobile App/Co-WIN_App.apk"
+mv -f "app-release.apk" "$HOME/Google Drive/Mobile App/Co-WIN_App.apk"
 
 flutter build apk --split-per-abi
-mv -f "C:/projects/my_projects/cowin_app/build/app/outputs/flutter-apk/app-x86_64-release.apk" "C:/Users/AnuragArwalkar/Google Drive/Mobile App/app-x86_64-release.apk"
-mv -f "C:/projects/my_projects/cowin_app/build/app/outputs/flutter-apk/app-armeabi-v7a-release.apk" "C:/Users/AnuragArwalkar/Google Drive/Mobile App/app-armeabi-v7a-release.apk"
-mv -f "C:/projects/my_projects/cowin_app/build/app/outputs/flutter-apk/app-arm64-v8a-release.apk" "C:/Users/AnuragArwalkar/Google Drive/Mobile App/app-arm64-v8a-release.apk"
+mv -f "app-x86_64-release.apk" "$HOME/Google Drive/Mobile App/app-x86_64-release.apk"
+mv -f "app-armeabi-v7a-release.apk" "$HOME/Google Drive/Mobile App/app-armeabi-v7a-release.apk"
+mv -f "app-arm64-v8a-release.apk" "$HOME/Google Drive/Mobile App/app-arm64-v8a-release.apk"
 
 echo "File copied successfully to Drive"
 read -p "Press any Key to exit..."
